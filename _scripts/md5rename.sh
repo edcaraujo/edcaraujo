e466686ce548b9ab3b5b4881b76e23780fd9d913
Eduardo Cruz Araujo <me@edcaraujo.com>
2019-09-05 20:51:21 -0300

    Add scripts

---
+++ _scripts/md5rename.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "md5rename v1.0.0: this script rename all files from current directory (recursively) to its 'md5sum'."
+echo
+echo "[#] Listing files..."
+echo
+find . -type f -exec bash -c 'echo $1 "=>" ${1%*/*}/$(md5sum $1 | sed "s/^\(\w*\)\s*\(.*\)/\1/").${1##*.}' bash {} \;
+echo 
+read -p "[!] Are you sure? [y/n]: " -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo
+  echo "[#] Moving files..."
+  echo
+  find . -type f -exec bash -c 'mv -v $1 ${1%*/*}/$(md5sum $1 | sed "s/^\(\w*\)\s*\(.*\)/\1/").${1##*.}' bash {} \;
+  echo
+fi
+echo "Bye!"
+
